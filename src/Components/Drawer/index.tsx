8b5233d55ec2875fed10c8194e57897b48bad224
Aditya <adityakho999@gmail.com>
2023-02-04 15:28:45 -0800

    Background change

--- src/Components/Drawer/index.tsx
+++ src/Components/Drawer/index.tsx
@@ -1,9 +1,18 @@
 import { Drawer } from "@mui/material";
 import DrawerContent from "./drawer-content";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+  paper: {
+    background: "#151419",
+    color: "white",
+  },
+});
 
 function Sidebar() {
+  const styles = useStyles();
   return (
-    <Drawer variant="permanent" anchor="left">
+    <Drawer variant="permanent" anchor="left" classes={{ paper: styles.paper }}>
       <DrawerContent />
     </Drawer>
   );

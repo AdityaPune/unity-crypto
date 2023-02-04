2e49732dbf02eb9de6e3364464ea5e0ffa2b5f8b
HarshOza36 <harshoza3636@gmail.com>
2023-02-04 15:53:40 -0800

    fix app

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,17 @@ import { store } from "./store";
 import { Provider } from "react-redux";
 import ViewBase from "./Components/ViewBase";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
-import { BorrowerDashboard, Dashboard, Login, Invest, BorrowerProfile, Portfolio } from "./Pages";
+
+import {
+  BorrowerDashboard,
+  Login,
+  Dashboard,
+  Invest,
+  BorrowerProfile,
+  Portfolio,
+} from "./Pages";
+
+
 import Order from "./Components/Order";
 
 function App() {

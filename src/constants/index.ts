ca23017e49cff26a232de735fdfdc978b98c1d40
Aditya <adityakho999@gmail.com>
2023-02-05 02:27:37 -0800

    Bunch of changes precommit

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,5 @@
 export * from "./blockchain";
 // export * from "./addresses";
 export const signatureType = "EIP712_SIGN";
+export { abi as contractAbi } from "./contract.json";
+export { abi as ercAbi } from "./ERC20.json";

839ef2cac84cbcdcb255a869797dcbb83cad32cd
Ashish Bhagchandani <ashishbhagchandani98@gmail.com>
2023-02-04 17:43:10 -0800

    add ashish

---
+++ src/Pages/Dashboard/dialog.tsx
@@ -0,0 +1,116 @@
+import * as React from "react";
+import Button from "@mui/material/Button";
+import { styled } from "@mui/material/styles";
+import Dialog from "@mui/material/Dialog";
+import DialogTitle from "@mui/material/DialogTitle";
+import DialogContent from "@mui/material/DialogContent";
+import DialogActions from "@mui/material/DialogActions";
+import IconButton from "@mui/material/IconButton";
+import CloseIcon from "@mui/icons-material/Close";
+import Typography from "@mui/material/Typography";
+import graph from "../../assets/images/agri.jpg";
+
+const BootstrapDialog = styled(Dialog)(({ theme }) => ({
+  "& .MuiDialogContent-root": {
+    padding: theme.spacing(2),
+    // height: "100%",
+    // width: "100%",
+  },
+  "& .MuiDialogActions-root": {
+    padding: theme.spacing(1),
+  },
+  "& .MuiPaper-root": {
+    height: "100%",
+    width: "100%",
+  },
+}));
+
+export interface DialogTitleProps {
+  id: string;
+  children?: React.ReactNode;
+  onClose: () => void;
+}
+
+function BootstrapDialogTitle(props: DialogTitleProps) {
+  const { children, onClose, ...other } = props;
+
+  return (
+    <DialogTitle sx={{ m: 0, p: 2 }} {...other}>
+      {children}
+      {onClose ? (
+        <IconButton
+          aria-label="close"
+          onClick={onClose}
+          sx={{
+            position: "absolute",
+            right: 8,
+            top: 8,
+            color: (theme) => theme.palette.grey[500],
+          }}
+        >
+          <CloseIcon />
+        </IconButton>
+      ) : null}
+    </DialogTitle>
+  );
+}
+
+export interface poolProps {
+  id: number;
+  name: string;
+  summary: string;
+}
+
+export default function CustomizedDialogs(pool: poolProps) {
+  const [open, setOpen] = React.useState(false);
+  const { name, summary, id } = pool;
+  //   console.log(name);
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <Button size="small" onClick={handleClickOpen}>
+        Open dialog
+      </Button>
+      <BootstrapDialog
+        onClose={handleClose}
+        aria-labelledby="customized-dialog-title"
+        open={open}
+      >
+        <BootstrapDialogTitle
+          id="customized-dialog-title"
+          onClose={handleClose}
+        >
+          {name}
+        </BootstrapDialogTitle>
+        <DialogContent dividers>
+          <div style={{ display: "flex", gap: "10px" }}>
+            <div>
+              <img style={{ width: "280px" }} src={graph}></img>
+            </div>
+            <div style={{ marginLeft: "40px" }}>
+              <p style={{ marginBottom: "0px" }}>Pool Name:</p>
+              <p style={{ marginTop: "0px" }}>Pool 1</p>
+              <p style={{ marginBottom: "0px" }}>Total active holding:</p>
+              <p style={{ marginTop: "0px" }}>$150k</p>
+              <p style={{ marginBottom: "0px" }}>Total Borrower:</p>
+              <p style={{ marginTop: "0px" }}>25</p>
+              <p style={{ marginBottom: "0px" }}>Pool type:</p>
+              <p style={{ marginTop: "0px" }}>Agriculture</p>
+            </div>
+          </div>
+        </DialogContent>
+        <DialogActions>
+          <Button autoFocus onClick={handleClose}>
+            Ok
+          </Button>
+        </DialogActions>
+      </BootstrapDialog>
+    </div>
+  );
+}
--- src/Pages/Dashboard/index.tsx
+++ src/Pages/Dashboard/index.tsx
@@ -12,6 +12,11 @@ import {
   CardContent,
   Card,
 } from "@mui/material";
+import * as React from "react";
+import CardMedia from "@mui/material/CardMedia";
+import poolimg from "../../assets/images/agri.jpg";
+import CustomizedDialogs from "./dialog";
+// import Grid from "@material-ui/core/Grid";
 
 import "./dashboard.scss";
 // import { useWeb3Context } from "../../hooks";
@@ -19,6 +24,41 @@ import "./dashboard.scss";
 import { Skeleton } from "@material-ui/lab";
 import classnames from "classnames";
 import ProductList from "../../Components/ProductList";
+import { styled } from "@mui/material/styles";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Pie } from "react-chartjs-2";
+// import { FaEllipsisH } from "react-icons/fa";
+
+// import _ from '../../assets/images/agri.jpg'
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+export const data = {
+  labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+  datasets: [
+    {
+      label: "# of Votes",
+      data: [12, 19, 3, 5, 2, 3],
+      backgroundColor: [
+        "rgba(255, 99, 132, 0.2)",
+        "rgba(54, 162, 235, 0.2)",
+        "rgba(255, 206, 86, 0.2)",
+        "rgba(75, 192, 192, 0.2)",
+        "rgba(153, 102, 255, 0.2)",
+        "rgba(255, 159, 64, 0.2)",
+      ],
+      borderColor: [
+        "rgba(255, 99, 132, 1)",
+        "rgba(54, 162, 235, 1)",
+        "rgba(255, 206, 86, 1)",
+        "rgba(75, 192, 192, 1)",
+        "rgba(153, 102, 255, 1)",
+        "rgba(255, 159, 64, 1)",
+      ],
+      borderWidth: 1,
+    },
+  ],
+};
 
 function Dashboard() {
   const dispatch = useDispatch();
@@ -35,6 +75,37 @@ function Dashboard() {
       </Typography>
     </CardContent>
   );
+
+  const pooldata = [
+    {
+      id: 1,
+      name: "pool1",
+      summary: "Summary about pool agenda, type, total amount and risk.",
+    },
+    {
+      id: 2,
+      name: "pool2",
+      summary: "Summary about pool agenda, type, total amount and risk.",
+    },
+    {
+      id: 3,
+      name: "pool3",
+      summary: "Summary about pool agenda, type, total amount and risk.",
+    },
+    {
+      id: 4,
+      name: "pool4",
+      summary: "Summary about pool agenda, type, total amount and risk.",
+    },
+  ];
+
+  // const Card = styled(card)(({ theme }) => ({
+  //   "& .MuiPaper-root": {
+  //     padding: theme.spacing(2),
+  //     // height: "100%",
+  //     width: "33%",
+  //   }
+  // }));
 
   return (
     <div className="dashboard-view">
@@ -61,7 +132,58 @@ function Dashboard() {
           </Box>
         </div>
       </div>
-      <ProductList />
+      {/* <ProductList /> */}
+      <div
+        style={{
+          height: "100%",
+          width: "100%",
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <div style={{ width: "25%", marginRight: "140px" }}>
+          <Card sx={{ maxWidth: 345 }}>
+            <Pie style={{ height: "200px", width: "200px" }} data={data} />
+          </Card>
+          <Card sx={{ maxWidth: 345 }}>
+            <Pie style={{ height: "200px", width: "200px" }} data={data} />
+          </Card>
+        </div>
+        <div className="pooldiv" style={{ width: "50%", marginLeft: "140px" }}>
+          <Grid container spacing={1}>
+            {pooldata.map((data, i) => {
+              return (
+                // <Grid container spacing={1}>
+                <Grid item xs={6} key={i}>
+                  <Card sx={{ maxWidth: 345 }}>
+                    <CardMedia
+                      sx={{ height: 140 }}
+                      image={poolimg}
+                      title="green iguana"
+                    />
+                    <CardContent>
+                      <Typography gutterBottom variant="h5" component="div">
+                        {data.name}
+                      </Typography>
+                      <Typography variant="body2" color="text.secondary">
+                        {data.summary}
+                      </Typography>
+                    </CardContent>
+                    <CardActions>
+                      <CustomizedDialogs
+                        id={data.id}
+                        name={data.name}
+                        summary={data.summary}
+                      ></CustomizedDialogs>
+                    </CardActions>
+                  </Card>
+                </Grid>
+                // </Grid>
+              );
+            })}
+          </Grid>
+        </div>
+      </div>
     </div>
   );
 }
